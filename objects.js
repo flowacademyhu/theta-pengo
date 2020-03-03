c173762176cbe84db59b62fc0c25a4a545183cd0
Viktor Mahler <vikitorony@gmail.com>
2020-03-03 15:50:13 +0100

    1st commit

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
-let floor = { type: 'floor', code: 0, symbol: ' ' };
-let ice = { type: 'iceblock', code: 1, symbol: 'J' };
-let enemy = {type: 'enemy', code: 2, symbol: '@' };
-let player = { type: 'player', code: 3, symbol: 'P', direction: 'up', xCoord: 0, yCoord: 0 };
+const floor = { type: 'floor', code: 0, symbol: ' ' };
+const ice = { type: 'iceblock', code: 1, symbol: 'J' };
+const enemy = { type: 'enemy', code: 2, symbol: '@' };
+const player = { type: 'player', code: 3, symbol: 'P', direction: 'up', xCoord: 0, yCoord: 0 };
 module.exports = { floor, ice, enemy, player };
