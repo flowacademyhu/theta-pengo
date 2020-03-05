c019e35855344c79551ae6ac4d4817cbf8d495ee
Viktor Mahler <vikitorony@gmail.com>
2020-03-05 15:04:47 +0100

    Axel.js

--- axel.js
+++ axel.js
@@ -1,4 +1,6 @@
-const ctx = require('axel');
+// Uses AXEL's node module
+var ctx = require('../index.js');
+
 /*
 // Clear the terminal
 ctx.clear();
@@ -35,19 +37,37 @@ ctx.clear();
 // the foreground color and the brush can be used to
 // add texture/detail to the pixel.
  */
-var brushes = ' ░▒▓█';
+// var brushes = ' ░▒▓█';
 
-function nextBrush (n) {
-  return brushes[parseInt(n % brushes.length)];
-}
-ctx.bg(128, 128, 128);
-// Draw some lines which step through the brush characters
-if ()
-  ctx.brush = nextBrush(y);
+// function nextBrush (n) {
+//   return brushes[parseInt(n % brushes.length)];
+// }
+// ctx.bg(128, 128, 128);
+// // Draw some lines which step through the brush characters
+// if ()
+//   ctx.brush = nextBrush(y);
 
-  // Each line is now draw with the specified
-  // character in each pixel
-  ctx.line(1, y, ctx.cols, y);
-}
+//   // Each line is now draw with the specified
+//   // character in each pixel
+//   ctx.line(1, y, ctx.cols, y);
+// }
 
+// ctx.clear();
+
+// Clear the terminal/screen/console
 ctx.clear();
+
+// Background to red, foreground to white
+ctx.bg(255, 0, 0);
+ctx.fg(255, 255, 255);
+ctx.text(5, 1, ' WHITE ON RED! ');
+
+// Background to yellow, foreground to black
+ctx.bg(255, 255, 0);
+ctx.fg(0, 0, 0);
+ctx.text(10, 2, ' BLACK ON YELLOW! ');
+
+
+// Position the cursor at the end of the console
+// so as not to draw over the bottom of the circle
+ctx.cursor.restore();
