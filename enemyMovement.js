0c1cc2971a40d0d92f14e1f6ef26c7b1349322e9
Viktor Mahler <vikitorony@gmail.com>
2020-03-11 16:51:23 +0100

    enemy tracks player - not finished

--- enemyMovement.js
+++ enemyMovement.js
@@ -47,53 +47,76 @@ const getAvailableDirections = (xCoord, yCoord, matrix) => { // getAvailableDire
 const filterAvailableDirections = (xCoord, yCoord, matrix) => { // el kell különíteni, hogy merre mehet, és mi merre akarjuk, hogy menjen
   const availableDirections = getAvailableDirections(xCoord, yCoord, matrix);
   const currentDirection = matrix[xCoord][yCoord].direction;
-  // console.log(xCoord, yCoord);
-  // console.log('availableDirections1:', availableDirections);
   const canGoFurther = availableDirections.includes(currentDirection);
-  // const arr = [up down left]; függvény az irányok kiszámítására
-  // for (let i = 0; i < availableDirections.length; i++) {
   if (availableDirections.includes(currentDirection)) {
     availableDirections.splice(availableDirections.indexOf(currentDirection), 1);
-    // console.log('after splice:', availableDirections);
   }
-  // console.log('oppositedir:', oppositeDirs[currentDirection]);
   if (availableDirections.includes(oppositeDirs[currentDirection])) {
     availableDirections.splice(availableDirections.indexOf(oppositeDirs[currentDirection]), 1);
-    // console.log('splice után ez marad: ', availableDirections);
   }
-  // console.log('availableDirections2:', availableDirections);
-  // }
   return { availableDirections, canGoFurther };
 };
 
 const moveEnemy = (xCoord, yCoord, matrix) => {
-  const { availableDirections, canGoFurther } = filterAvailableDirections(xCoord, yCoord, matrix);
-  //
-  // console.log('currentDirection:', matrix[xCoord][yCoord].direction);
   let newCoord = [];
+  const eatYouAlive = iWillEatYou(xCoord, yCoord, matrix);
+  if (eatYouAlive) {
+    newCoord = stepTo(matrix, xCoord, yCoord, eatYouAlive);
+    return `${newCoord[0]}${newCoord[1]}`;
+  }
+
+  const { availableDirections, canGoFurther } = filterAvailableDirections(xCoord, yCoord, matrix);
   if (availableDirections.length === 0 && canGoFurther) { // 1 lépést előre --- így átmegy mindenen ki a pályáról
-    // console.log('ÜRES A tömb, megy tovább');
     const currentDirection = matrix[xCoord][yCoord].direction;
     newCoord = stepTo(matrix, xCoord, yCoord, currentDirection); // kell vizsgálni, hogy szabad-e lépni abba az irányba
   } else {
-    // console.log(availableDirections);
     const randomIndex = Math.floor(Math.random() * availableDirections.length); // lehet kell neki +1? Nem
     const newDirection = availableDirections[randomIndex];
-    // console.log('random irány:', newDirection);
     matrix[xCoord][yCoord].direction = newDirection;
     newCoord = stepTo(matrix, xCoord, yCoord, newDirection);
-
-    // Random irány
-    // const randomIndex = Math.random()*availableDirections.length;
-    // const newDirection = arr[randomIndex]
-    // stepTo(newDirection)
   }
   return `${newCoord[0]}${newCoord[1]}`;
 };
 
-// max 4 for-ral fgv, ami megkapja a szörny valid irányait és az alapján for up, ha fal -> break, ha játékos, akkor arra megy. getAvailableDirections adja a for-ok alapirányait, és végül irányt adunk vissza
+// enemy kövesse a playert, ha látja a folyosón
+// max 4 for-ral fgv, ami megkapja a szörny valid irányait és az alapján for up keresés, ha fal -> break, ha játékos, akkor arra megy. getAvailableDirections adja a for-ok alapirányait, és végül irányt adunk vissza
 // ha irány UP, for x/y tengelyen
 
-/* if (matrix[xCoord][yCoord].direction === ) */ // ha az utolsó direction irányába tud még lépni, akkora arra lépjen még egyet
+const iWillEatYou = (xCoord, yCoord, matrix) => {
+  const availableDirs = getAvailableDirections(xCoord, yCoord, matrix);
+  if (availableDirs.includes('up')) {
+    for (let x = xCoord; x >= 0; x--) {
+      if (matrix[x][yCoord].type === 'player') {
+        return 'up';
+      } else if (matrix[x][yCoord].type === 'iceblock') {
+        break;
+      }
+    }
+  } else if (availableDirs.includes('down')) {
+    for (let x = xCoord; x <= 0; x++) {
+      if (matrix[x][yCoord].type === 'player') {
+        return 'down';
+      } else if (matrix[x][yCoord].type === 'iceblock') {
+        break;
+      }
+    }
+  } else if (availableDirs.includes('left')) {
+    for (let y = yCoord; y >= 0; y--) {
+      if (matrix[xCoord][y].type === 'player') {
+        return 'left';
+      } else if (matrix[xCoord][y].type === 'iceblock') {
+        break;
+      }
+    }
+  } else if (availableDirs.includes('right')) {
+    for (let y = yCoord; y <= 0; y++) {
+      if (matrix[xCoord][y].type === 'player') {
+        return 'right';
+      } else if (matrix[xCoord][y].type === 'iceblock') {
+        break;
+      }
+    }
+  }
+};
 
-module.exports = { moveEnemy, filterAvailableDirections };
+module.exports = { moveEnemy, filterAvailableDirections, iWillEatYou };
